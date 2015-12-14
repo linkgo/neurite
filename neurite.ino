79a6e484f2c3a0999664f463a4e42f638f06e3c0
Librae <librae8226@gmail.com>
2015-12-14 17:57:45 +0800

    basic infrastucture ready

--- neurite.ino
+++ neurite.ino
@@ -32,24 +32,66 @@
 #endif
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
+#include <PubSubClient.h>
 #include <Ticker.h>
 
-#define SSID1		"ssid1"
-#define PSK1		"psk1"
-#define SSID2		"ssid2"
-#define PSK2		"psk2"
+extern "C" {
+#include "osapi.h"
+#include "ets_sys.h"
+#include "user_interface.h"
+}
+
+#define NEURITE_CMD_BUF_SIZE		256
+#define NEURITE_UID_LEN			32
+#define NEURITE_TOPIC_LEN		64
 
 #define NEURITE_LED	13
 #define NEURITE_BUTTON	0
+
+#define __bzero(m,s)	memset(m,0,s)
+
+struct cmd_parser_s;
+typedef void (*cmd_parser_cb_fp)(struct cmd_parser_s *cp);
+
+struct cmd_parser_s {
+	char *buf;
+	uint16_t buf_size;
+	uint16_t data_len;
+	bool cmd_begin;
+	cmd_parser_cb_fp callback;
+};
+
+struct neurite_mqtt_cfg_s {
+	char uid[NEURITE_UID_LEN];
+	char topic_to[NEURITE_TOPIC_LEN];
+	char topic_from[NEURITE_TOPIC_LEN];
+};
+
+struct neurite_data_s {
+	bool wifi_connected;
+	bool mqtt_connected;
+	struct neurite_mqtt_cfg_s nmcfg;
+	struct cmd_parser_s *cp;
+};
+
+struct neurite_data_s g_nd;
+struct cmd_parser_s g_cp;
+
+#if 0
+RINGBUF cmd_rx_rb;
+char cmd_rx_buf[256];
+#endif
+char cmd_buf[NEURITE_CMD_BUF_SIZE];
 
 #ifdef WIFI_CONFIG_MULTI
 ESP8266WiFiMulti WiFiMulti;
 #endif
 Ticker ticker_worker;
 Ticker ticker_led;
+WiFiClient wifi_cli;
+PubSubClient mqtt_cli(wifi_cli);
 
-//static char *ota_url = "http://accrete.org:8080/firmware/esp.bin";
-static char *ota_url = "http://192.168.100.154/firmware/esp.bin";
+static char uid[32];
 
 enum {
 	WORKER_ST_0 = 0,
@@ -91,32 +133,31 @@ static void ticker_led_blink(void)
 }
 
 #ifndef WIFI_CONFIG_MULTI
-static int setup_wifi(char *ssid, char *password)
+static int wifi_connect(char *ssid, char *password)
 {
 	if (!ssid || !password) {
 		log_err("invalid args\n\r");
 		return -1;
 	}
 
-	Serial.println();
-	Serial.print("Connecting to ");
+	log_dbg("Connecting to ");
 	Serial.println(ssid);
 
 	WiFi.begin(ssid, password);
 }
 #endif
 
-static inline void config_wifi(void)
+static inline void wifi_config(void)
 {
 #ifdef WIFI_CONFIG_MULTI
 	WiFiMulti.addAP(SSID1, PSK1);
 	WiFiMulti.addAP(SSID2, PSK2);
 #else
-	setup_wifi(SSID1, PSK1);
+	wifi_connect(SSID1, PSK1);
 #endif
 }
 
-static inline bool check_wifi_status(void)
+static inline bool wifi_check_status(void)
 {
 #ifdef WIFI_CONFIG_MULTI
 	return (WiFiMulti.run() == WL_CONNECTED);
@@ -152,7 +193,54 @@ static int ota_over_http(char *url)
 	return ret;
 }
 
-static void neurite_child_worker(void)
+static void mqtt_callback(char *topic, byte *payload, unsigned int length)
+{
+	log_dbg("topic (%d): %s\n\r", strlen(topic), topic);
+	log_dbg("data (%d): ", length);
+	for (int i = 0; i < length; i++)
+		USE_SERIAL.print((char)payload[i]);
+	USE_SERIAL.println();
+}
+
+static inline bool mqtt_check_status(void)
+{
+	return mqtt_cli.connected();
+}
+
+static void mqtt_config(void)
+{
+	mqtt_cli.setServer(MQTT_SERVER, 1883);
+	mqtt_cli.setCallback(mqtt_callback);
+}
+
+static void mqtt_connect(struct neurite_data_s *nd)
+{
+	mqtt_config();
+	mqtt_cli.connect(nd->nmcfg.uid);
+}
+
+static void cmd_completed_cb(struct cmd_parser_s *cp)
+{
+	dbg_assert(cp);
+	if (cp->data_len > 0) {
+		log_dbg("msg launch(len %d): %s\n\r", cp->data_len, cp->buf);
+//		MQTT_Publish(&g_nd.mc, g_nd.nmcfg.topic_to, cp->buf, cp->data_len, 0, 0);
+	}
+	__bzero(cp->buf, cp->buf_size);
+	cp->data_len = 0;
+}
+
+int cmd_parser_init(struct cmd_parser_s *cp, char *buf, uint16_t buf_size)
+{
+	dbg_assert(cp);
+	cp->buf = buf;
+	cp->buf_size = buf_size;
+	cp->data_len = 0;
+	cp->callback = cmd_completed_cb;
+	return 0;
+}
+
+void neurite_child_worker(void)
 {
 	log_dbg("my time is: %d ms\n\r", millis());
 }
@@ -167,16 +255,17 @@ static inline void stop_child_worker(void)
 	ticker_worker.detach();
 }
 
-static inline void neurite_worker(void)
+inline void neurite_worker(void)
 {
+	struct neurite_data_s *nd = &g_nd;
 	switch (worker_st) {
 		case WORKER_ST_0:
 			ticker_led.attach_ms(50, ticker_led_blink);
-			config_wifi();
+			wifi_config();
 			update_worker_state(WORKER_ST_1);
 			break;
 		case WORKER_ST_1:
-			if (!check_wifi_status())
+			if (!wifi_check_status())
 				break;
 
 			log_dbg("WiFi connected\n\r");
@@ -185,30 +274,28 @@ static inline void neurite_worker(void)
 
 			if (digitalRead(NEURITE_BUTTON) == LOW) {
 				ticker_led.detach();
-				ota_over_http(ota_url);
+				ota_over_http(OTA_URL_DEFAULT);
 			}
-#if 0
-			neurite_mqtt_connect(nd);
-#endif
 			update_worker_state(WORKER_ST_2);
 			break;
 		case WORKER_ST_2:
-#if 0
-			if (!nd->mqtt_connected)
+			if (!mqtt_check_status()) {
+				mqtt_connect(nd);
 				break;
-			MQTT_Subscribe(&nd->mc, nd->nmcfg.topic_from, 1);
-			uint8_t *payload_buf = (uint8_t *)os_malloc(32);
+			}
+			mqtt_cli.subscribe(nd->nmcfg.topic_from);
+			char payload_buf[32];
 			dbg_assert(payload_buf);
-			os_sprintf(payload_buf, "checkin: %s", nd->nmcfg.uid);
-			MQTT_Publish(&nd->mc, nd->nmcfg.topic_to, payload_buf, strlen(payload_buf), 1, 0);
-			os_free(payload_buf);
-#endif
+			sprintf(payload_buf, "checkin: %s", nd->nmcfg.uid);
+			mqtt_cli.publish(nd->nmcfg.topic_to, (const char *)payload_buf);
+
 			ticker_led.detach();
 			ticker_led.attach_ms(50, ticker_led_breath);
 			start_child_worker();
 			update_worker_state(WORKER_ST_3);
 			break;
 		case WORKER_ST_3:
+			mqtt_cli.loop();
 			break;
 		default:
 			log_err("unknown worker state: %d\n\r", worker_st);
@@ -216,15 +303,61 @@ static inline void neurite_worker(void)
 	}
 }
 
+void neurite_cmd_init(struct neurite_data_s *nd)
+{
+}
+
+void neurite_init(void)
+{
+	struct neurite_data_s *nd = &g_nd;
+	log_dbg("in\n\r");
+
+	__bzero(nd, sizeof(struct neurite_data_s));
+	__bzero(&g_cp, sizeof(struct cmd_parser_s));
+	nd->cp = &g_cp;
+	__bzero(cmd_buf, NEURITE_CMD_BUF_SIZE);
+	cmd_parser_init(nd->cp, cmd_buf, NEURITE_CMD_BUF_SIZE);
+
+	/* TODO these items need to be configured at runtime */
+	__bzero(&nd->nmcfg, sizeof(struct neurite_mqtt_cfg_s));
+	sprintf(nd->nmcfg.uid, "neurite-%08x", ESP.getChipId());
+#if 0
+	sprintf(nd->nmcfg.topic_to, "/neuro/%s/to", nd->nmcfg.uid);
+	sprintf(nd->nmcfg.topic_from, "/neuro/%s/to", nd->nmcfg.uid);
+#else
+	sprintf(nd->nmcfg.topic_to, "/neuro/chatroom", nd->nmcfg.uid);
+	sprintf(nd->nmcfg.topic_from, "/neuro/chatroom", nd->nmcfg.uid);
+#endif
+#if 0
+	sprintf(nd->cfg->sta_ssid, "%s", STA_SSID);
+	sprintf(nd->cfg->sta_pwd, "%s", STA_PASS);
+#endif
+	log_dbg("chip id: %08x\n\r", system_get_chip_id());
+	log_dbg("uid: %s\n\r", nd->nmcfg.uid);
+	log_dbg("topic_to: %s\n\r", nd->nmcfg.topic_to);
+	log_dbg("topic_from: %s\n\r", nd->nmcfg.topic_from);
+#if 0
+	log_dbg("device id: %s\n\r", nd->cfg->device_id);
+	log_dbg("ssid: %s\n\r", nd->cfg->sta_ssid);
+	log_dbg("psk : %s\n\r", nd->cfg->sta_pwd);
+	log_dbg("mqtt user: %s\n\r", nd->cfg->mqtt_user);
+	log_dbg("mqtt pass: %s\n\r", nd->cfg->mqtt_pass);
+#endif
+	neurite_cmd_init(nd);
+
+	log_dbg("out\n\r");
+}
+
 void setup()
 {
 	USE_SERIAL.begin(115200);
-//	USE_SERIAL.setDebugOutput(true);
+	USE_SERIAL.setDebugOutput(false);
 	USE_SERIAL.printf("\n\r");
 	USE_SERIAL.flush();
 	pinMode(NEURITE_LED, OUTPUT);
 	digitalWrite(NEURITE_LED, HIGH);
 	pinMode(NEURITE_BUTTON, INPUT);
+	neurite_init();
 }
 
 void loop()
