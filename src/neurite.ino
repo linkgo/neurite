21c93960ad73565cef5edcc7637677b431978f89
Librae <librae8226@gmail.com>
2016-01-23 02:34:28 +0800

    make lcd work and support bitmap logo

--- src/neurite.ino
+++ src/neurite.ino
@@ -1191,6 +1191,13 @@ enum {
 };
 static int user_st = USER_ST_0;
 
+#define TFT_CS     15 // tied to ground
+#define TFT_RST    3
+#define TFT_DC     1
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+
 static inline void update_user_state(int st)
 {
 	log_dbg("-> USER_ST_%d\n\r", st);
@@ -1250,6 +1257,21 @@ void neurite_user_mqtt(char *topic, byte *payload, unsigned int length)
 		}
 	}
 	if (strcmp(topic, nd->cfg.topic_from) == 0) {
+		int16_t y = tft.getCursorY();
+		if ((y + 8) >= ST7735_TFTHEIGHT_144) {
+			y = 0;
+			tft.fillScreen(ST7735_BLACK);
+		} else {
+			y = y + 8;
+		}
+		tft.setCursor(0, y);
+		tft.setTextColor(ST7735_WHITE);
+		tft.setTextWrap(true);
+		char text[MQTT_MSG_LEN];
+		__bzero(text, sizeof(text));
+		for (int i = 0; i < length; i++)
+			text[i] = payload[i];
+		tft.print((char *)text);
 	}
 }
 
@@ -1269,32 +1291,6 @@ void neurite_user_button(int time_ms)
 				mqtt_cli.publish(nd->cfg.topic_to, "light off");
 		}
 	}
-}
-
-// For the breakout, you can use any 2 or 3 pins
-// These pins will also work for the 1.8" TFT shield
-#define TFT_CS     15
-#define TFT_RST    3  // you can also connect this to the Arduino reset
-                      // in which case, set this #define pin to 0!
-#define TFT_DC     1
-
-// Option 1 (recommended): must use the hardware SPI pins
-// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
-// an output. This is much faster - also required if you want
-// to use the microSD card (see the image drawing example)
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
-
-// Option 2: use any pins but a little slower!
-#define TFT_SCLK 14   // set these to be whatever pins you like!
-#define TFT_MOSI 13   // set these to be whatever pins you like!
-//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
-
-static void testdrawtext(char *text, uint16_t color) {
-  tft.setCursor(0, 0);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
 }
 
 /* will be called after neurite system setup */
@@ -1319,9 +1315,156 @@ void neurite_user_setup(void)
 
 	// large block of text
 	tft.fillScreen(ST7735_BLACK);
-	testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST7735_WHITE);
-	delay(1000);
 
-	ticker_user.attach_ms(1000, ticker_user_task);
+	bmpDraw("/logo.bmp", 39, 0);
+
+	tft.setCursor(0, 8*7);
+	tft.setTextColor(ST7735_WHITE);
+	tft.print("  Made with love by  ");
+	tft.setCursor(0, 8*8);
+	tft.print("      linkgo.");
+	tft.setTextColor(ST7735_RED);
+	tft.print("i");
+	tft.setTextColor(ST7735_GREEN);
+	tft.print("o");
+	tft.setCursor(0, 8*10);
+	delay(2000);
+}
+
+#define BUFFPIXEL 20
+
+void bmpDraw(char *filename, uint8_t x, uint8_t y) {
+
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0, startTime = millis();
+
+  if((x >= tft.width()) || (y >= tft.height())) return;
+
+  LOG_SERIAL.println();
+  LOG_SERIAL.print("Loading image '");
+  LOG_SERIAL.print(filename);
+  LOG_SERIAL.println('\'');
+
+  // Open requested file on SD card
+  if ((bmpFile = SPIFFS.open(filename, "r")) == NULL) {
+    LOG_SERIAL.print("File not found");
+    //mqtt_cli.publish("/neuro/chatroom", "file not found");
+    return;
+  }
+
+  // Parse BMP header
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    //mqtt_cli.publish("/neuro/chatroom", "bmp signature");
+    LOG_SERIAL.print("File size: "); LOG_SERIAL.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile); // Start of image data
+    LOG_SERIAL.print("Image Offset: "); LOG_SERIAL.println(bmpImageoffset, DEC);
+    // Read DIB header
+    LOG_SERIAL.print("Header size: "); LOG_SERIAL.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
+      bmpDepth = read16(bmpFile); // bits per pixel
+      LOG_SERIAL.print("Bit Depth: "); LOG_SERIAL.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+
+        goodBmp = true; // Supported BMP format -- proceed!
+        LOG_SERIAL.print("Image size: ");
+        LOG_SERIAL.print(bmpWidth);
+        LOG_SERIAL.print('x');
+        LOG_SERIAL.println(bmpHeight);
+
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * 3 + 3) & ~3;
+
+        // If bmpHeight is negative, image is in top-down order.
+        // This is not canon but has been observed in the wild.
+        if(bmpHeight < 0) {
+          bmpHeight = -bmpHeight;
+          flip      = false;
+        }
+
+        // Crop area to be loaded
+        w = bmpWidth;
+        h = bmpHeight;
+        if((x+w-1) >= tft.width())  w = tft.width()  - x;
+        if((y+h-1) >= tft.height()) h = tft.height() - y;
+
+        // Set TFT address window to clipped image bounds
+        tft.setAddrWindow(x, y, x+w-1, y+h-1);
+
+        for (row=0; row<h; row++) { // For each scanline...
+
+          // Seek to start of scan line.  It might seem labor-
+          // intensive to be doing this on every line, but this
+          // method covers a lot of gritty details like cropping
+          // and scanline padding.  Also, the seek only takes
+          // place if the file position actually needs to change
+          // (avoids a lot of cluster math in SD library).
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+          else     // Bitmap is stored top-to-bottom
+            pos = bmpImageoffset + row * rowSize;
+          if(bmpFile.position() != pos) { // Need seek?
+            bmpFile.seek(pos, SeekSet);
+            buffidx = sizeof(sdbuffer); // Force buffer reload
+          }
+
+          for (col=0; col<w; col++) { // For each pixel...
+            // Time to read more pixel data?
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
+              bmpFile.read(sdbuffer, sizeof(sdbuffer));
+              buffidx = 0; // Set index to beginning
+            }
+
+            // Convert pixel from BMP to TFT format, push to display
+            b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+            tft.pushColor(tft.Color565(r,g,b));
+          } // end pixel
+        } // end scanline
+        LOG_SERIAL.print("Loaded in ");
+        LOG_SERIAL.print(millis() - startTime);
+        LOG_SERIAL.println(" ms");
+      } // end goodBmp
+    }
+  }
+
+  bmpFile.close();
+  if(!goodBmp) {
+    LOG_SERIAL.println("BMP format not recognized.");
+    //mqtt_cli.publish("/neuro/chatroom", "not recognized");
+  }
+}
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File f) {
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(File f) {
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
 }
 #endif
