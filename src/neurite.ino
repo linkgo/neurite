f00dc0dbf8cae909218bb1c0860812b43314a4bd
Librae <librae8226@gmail.com>
2016-03-31 21:32:09 +0800

    rebase master

--- src/neurite.ino
+++ src/neurite.ino
@@ -1197,7 +1197,6 @@ static int user_st = USER_ST_0;
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
-
 static inline void update_user_state(int st)
 {
 	log_dbg("-> USER_ST_%d\n\r", st);
@@ -1236,12 +1235,6 @@ void neurite_user_hold(void)
 {
 	log_dbg("\n\r");
 	update_user_state(USER_ST_0);
-}
-
-/* will be called after neurite system setup */
-void neurite_user_setup(void)
-{
-	log_dbg("\n\r");
 }
 
 /* called once on mqtt message received */
@@ -1335,117 +1328,117 @@ void neurite_user_setup(void)
 
 void bmpDraw(char *filename, uint8_t x, uint8_t y) {
 
-  File     bmpFile;
-  int      bmpWidth, bmpHeight;   // W+H in pixels
-  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
-  uint32_t bmpImageoffset;        // Start of image data in file
-  uint32_t rowSize;               // Not always = bmpWidth; may have padding
-  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
-  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
-  boolean  goodBmp = false;       // Set to true on valid header parse
-  boolean  flip    = true;        // BMP is stored bottom-to-top
-  int      w, h, row, col;
-  uint8_t  r, g, b;
-  uint32_t pos = 0, startTime = millis();
+	File     bmpFile;
+	int      bmpWidth, bmpHeight;   // W+H in pixels
+	uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+	uint32_t bmpImageoffset;        // Start of image data in file
+	uint32_t rowSize;               // Not always = bmpWidth; may have padding
+	uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+	uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+	boolean  goodBmp = false;       // Set to true on valid header parse
+	boolean  flip    = true;        // BMP is stored bottom-to-top
+	int      w, h, row, col;
+	uint8_t  r, g, b;
+	uint32_t pos = 0, startTime = millis();
 
-  if((x >= tft.width()) || (y >= tft.height())) return;
+	if((x >= tft.width()) || (y >= tft.height())) return;
 
-  LOG_SERIAL.println();
-  LOG_SERIAL.print("Loading image '");
-  LOG_SERIAL.print(filename);
-  LOG_SERIAL.println('\'');
+	LOG_SERIAL.println();
+	LOG_SERIAL.print("Loading image '");
+	LOG_SERIAL.print(filename);
+	LOG_SERIAL.println('\'');
 
-  // Open requested file on SD card
-  if ((bmpFile = SPIFFS.open(filename, "r")) == NULL) {
-    LOG_SERIAL.print("File not found");
-    //mqtt_cli.publish("/neuro/chatroom", "file not found");
-    return;
-  }
+	// Open requested file on SD card
+	if ((bmpFile = SPIFFS.open(filename, "r")) == NULL) {
+		LOG_SERIAL.print("File not found");
+		//mqtt_cli.publish("/neuro/chatroom", "file not found");
+		return;
+	}
 
-  // Parse BMP header
-  if(read16(bmpFile) == 0x4D42) { // BMP signature
-    //mqtt_cli.publish("/neuro/chatroom", "bmp signature");
-    LOG_SERIAL.print("File size: "); LOG_SERIAL.println(read32(bmpFile));
-    (void)read32(bmpFile); // Read & ignore creator bytes
-    bmpImageoffset = read32(bmpFile); // Start of image data
-    LOG_SERIAL.print("Image Offset: "); LOG_SERIAL.println(bmpImageoffset, DEC);
-    // Read DIB header
-    LOG_SERIAL.print("Header size: "); LOG_SERIAL.println(read32(bmpFile));
-    bmpWidth  = read32(bmpFile);
-    bmpHeight = read32(bmpFile);
-    if(read16(bmpFile) == 1) { // # planes -- must be '1'
-      bmpDepth = read16(bmpFile); // bits per pixel
-      LOG_SERIAL.print("Bit Depth: "); LOG_SERIAL.println(bmpDepth);
-      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+	// Parse BMP header
+	if(read16(bmpFile) == 0x4D42) { // BMP signature
+		//mqtt_cli.publish("/neuro/chatroom", "bmp signature");
+		LOG_SERIAL.print("File size: "); LOG_SERIAL.println(read32(bmpFile));
+		(void)read32(bmpFile); // Read & ignore creator bytes
+		bmpImageoffset = read32(bmpFile); // Start of image data
+		LOG_SERIAL.print("Image Offset: "); LOG_SERIAL.println(bmpImageoffset, DEC);
+		// Read DIB header
+		LOG_SERIAL.print("Header size: "); LOG_SERIAL.println(read32(bmpFile));
+		bmpWidth  = read32(bmpFile);
+		bmpHeight = read32(bmpFile);
+		if(read16(bmpFile) == 1) { // # planes -- must be '1'
+			bmpDepth = read16(bmpFile); // bits per pixel
+			LOG_SERIAL.print("Bit Depth: "); LOG_SERIAL.println(bmpDepth);
+			if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
 
-        goodBmp = true; // Supported BMP format -- proceed!
-        LOG_SERIAL.print("Image size: ");
-        LOG_SERIAL.print(bmpWidth);
-        LOG_SERIAL.print('x');
-        LOG_SERIAL.println(bmpHeight);
+				goodBmp = true; // Supported BMP format -- proceed!
+				LOG_SERIAL.print("Image size: ");
+				LOG_SERIAL.print(bmpWidth);
+				LOG_SERIAL.print('x');
+				LOG_SERIAL.println(bmpHeight);
 
-        // BMP rows are padded (if needed) to 4-byte boundary
-        rowSize = (bmpWidth * 3 + 3) & ~3;
+				// BMP rows are padded (if needed) to 4-byte boundary
+				rowSize = (bmpWidth * 3 + 3) & ~3;
 
-        // If bmpHeight is negative, image is in top-down order.
-        // This is not canon but has been observed in the wild.
-        if(bmpHeight < 0) {
-          bmpHeight = -bmpHeight;
-          flip      = false;
-        }
+				// If bmpHeight is negative, image is in top-down order.
+				// This is not canon but has been observed in the wild.
+				if(bmpHeight < 0) {
+					bmpHeight = -bmpHeight;
+					flip      = false;
+				}
 
-        // Crop area to be loaded
-        w = bmpWidth;
-        h = bmpHeight;
-        if((x+w-1) >= tft.width())  w = tft.width()  - x;
-        if((y+h-1) >= tft.height()) h = tft.height() - y;
+				// Crop area to be loaded
+				w = bmpWidth;
+				h = bmpHeight;
+				if((x+w-1) >= tft.width())  w = tft.width()  - x;
+				if((y+h-1) >= tft.height()) h = tft.height() - y;
 
-        // Set TFT address window to clipped image bounds
-        tft.setAddrWindow(x, y, x+w-1, y+h-1);
+				// Set TFT address window to clipped image bounds
+				tft.setAddrWindow(x, y, x+w-1, y+h-1);
 
-        for (row=0; row<h; row++) { // For each scanline...
+				for (row=0; row<h; row++) { // For each scanline...
 
-          // Seek to start of scan line.  It might seem labor-
-          // intensive to be doing this on every line, but this
-          // method covers a lot of gritty details like cropping
-          // and scanline padding.  Also, the seek only takes
-          // place if the file position actually needs to change
-          // (avoids a lot of cluster math in SD library).
-          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
-            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
-          else     // Bitmap is stored top-to-bottom
-            pos = bmpImageoffset + row * rowSize;
-          if(bmpFile.position() != pos) { // Need seek?
-            bmpFile.seek(pos, SeekSet);
-            buffidx = sizeof(sdbuffer); // Force buffer reload
-          }
+					// Seek to start of scan line.  It might seem labor-
+					// intensive to be doing this on every line, but this
+					// method covers a lot of gritty details like cropping
+					// and scanline padding.  Also, the seek only takes
+					// place if the file position actually needs to change
+					// (avoids a lot of cluster math in SD library).
+					if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+						pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+					else     // Bitmap is stored top-to-bottom
+						pos = bmpImageoffset + row * rowSize;
+					if(bmpFile.position() != pos) { // Need seek?
+						bmpFile.seek(pos, SeekSet);
+						buffidx = sizeof(sdbuffer); // Force buffer reload
+					}
 
-          for (col=0; col<w; col++) { // For each pixel...
-            // Time to read more pixel data?
-            if (buffidx >= sizeof(sdbuffer)) { // Indeed
-              bmpFile.read(sdbuffer, sizeof(sdbuffer));
-              buffidx = 0; // Set index to beginning
-            }
+					for (col=0; col<w; col++) { // For each pixel...
+						// Time to read more pixel data?
+						if (buffidx >= sizeof(sdbuffer)) { // Indeed
+							bmpFile.read(sdbuffer, sizeof(sdbuffer));
+							buffidx = 0; // Set index to beginning
+						}
 
-            // Convert pixel from BMP to TFT format, push to display
-            b = sdbuffer[buffidx++];
-            g = sdbuffer[buffidx++];
-            r = sdbuffer[buffidx++];
-            tft.pushColor(tft.Color565(r,g,b));
-          } // end pixel
-        } // end scanline
-        LOG_SERIAL.print("Loaded in ");
-        LOG_SERIAL.print(millis() - startTime);
-        LOG_SERIAL.println(" ms");
-      } // end goodBmp
-    }
-  }
+						// Convert pixel from BMP to TFT format, push to display
+						b = sdbuffer[buffidx++];
+						g = sdbuffer[buffidx++];
+						r = sdbuffer[buffidx++];
+						tft.pushColor(tft.Color565(r,g,b));
+					} // end pixel
+				} // end scanline
+				LOG_SERIAL.print("Loaded in ");
+				LOG_SERIAL.print(millis() - startTime);
+				LOG_SERIAL.println(" ms");
+			} // end goodBmp
+		}
+	}
 
-  bmpFile.close();
-  if(!goodBmp) {
-    LOG_SERIAL.println("BMP format not recognized.");
-    //mqtt_cli.publish("/neuro/chatroom", "not recognized");
-  }
+	bmpFile.close();
+	if(!goodBmp) {
+		LOG_SERIAL.println("BMP format not recognized.");
+		//mqtt_cli.publish("/neuro/chatroom", "not recognized");
+	}
 }
 
 // These read 16- and 32-bit types from the SD card file.
@@ -1453,18 +1446,18 @@ void bmpDraw(char *filename, uint8_t x, uint8_t y) {
 // May need to reverse subscript order if porting elsewhere.
 
 uint16_t read16(File f) {
-  uint16_t result;
-  ((uint8_t *)&result)[0] = f.read(); // LSB
-  ((uint8_t *)&result)[1] = f.read(); // MSB
-  return result;
+	uint16_t result;
+	((uint8_t *)&result)[0] = f.read(); // LSB
+	((uint8_t *)&result)[1] = f.read(); // MSB
+	return result;
 }
 
 uint32_t read32(File f) {
-  uint32_t result;
-  ((uint8_t *)&result)[0] = f.read(); // LSB
-  ((uint8_t *)&result)[1] = f.read();
-  ((uint8_t *)&result)[2] = f.read();
-  ((uint8_t *)&result)[3] = f.read(); // MSB
-  return result;
+	uint32_t result;
+	((uint8_t *)&result)[0] = f.read(); // LSB
+	((uint8_t *)&result)[1] = f.read();
+	((uint8_t *)&result)[2] = f.read();
+	((uint8_t *)&result)[3] = f.read(); // MSB
+	return result;
 }
 #endif
