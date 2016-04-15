5aac56938bce2b58729004974258d8ff35deb904
Librae <librae8226@gmail.com>
2016-04-15 10:36:55 +0800

    clean up experiment stuff

--- src/neurite.ino
+++ src/neurite.ino
@@ -1319,35 +1319,6 @@ void neurite_user_button(int time_ms)
 			else
 				mqtt_cli.publish("/neuro/chatroom", "light off");
 		}
-#if 0
-		/* some experiment */
-		WiFi.mode(WIFI_STA);
-		WiFi.disconnect();
-		delay(100);
-		Serial.println("setup done");
-		int n = WiFi.scanNetworks(false, true);
-		Serial.println("scan done");
-		if (n == 0)
-			Serial.println("no networks found");
-		else
-		{
-			Serial.print(n);
-			Serial.println(" networks found");
-			for (int i = 0; i < n; ++i)
-			{
-				// Print SSID and RSSI for each network found
-				Serial.print(i + 1);
-				Serial.print(": ");
-				Serial.print(WiFi.SSID(i));
-				Serial.print(" (");
-				Serial.print(WiFi.RSSI(i));
-				Serial.print(")");
-				Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
-				delay(10);
-			}
-		}
-		Serial.println("");
-#endif
 	}
 }
 #endif
